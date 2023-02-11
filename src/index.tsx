3bcb9d190121b9836c3873b8d9408ac08eac00ca
amejid <abdelmejid89@gmail.com>
2023-02-11 15:36:43 +0300

    Cleanup

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return <div></div>;
+};
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(<App />);
